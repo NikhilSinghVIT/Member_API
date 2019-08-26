688dc5732933d162e5a8f3b6b8e79afa002ec5b6
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2019-08-27 01:36:28 +0530

    added feature to delete records and authorization

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask,g,request,jsonify
 from database import get_db
+from functools import wraps
 
 app = Flask(__name__)
+
+api_username = 'admin'
+api_password = 'password'
+
+def protected(f):
+	@wraps(f)
+	def decorated(*args,**kwargs):
+		auth = request.authorization
+		if auth and auth.username == api_username and auth.password == api_password:
+			return f(*args,**kwargs)
+		return jsonify({'message':'Authentication failed!'}),403
+	return decorated
 
 @app.teardown_appcontext
 def close_db(error):
@@ -9,6 +22,7 @@ def close_db(error):
 		g.sqlite_db.close()
 
 @app.route('/member',methods=['GET'])
+@protected
 def get_members():
 	db = get_db()
 	members_cur = db.execute('select id ,name,email,level from members')
@@ -28,6 +42,7 @@ def get_members():
 	return jsonify({"member":return_values_list})
 
 @app.route('/member/<int:member_id>',methods=['GET'])
+@protected
 def get_member(member_id):
 	db = get_db()
 	member_cur = db.execute('select id,name,email,level from members where id=?',[member_id])
@@ -37,6 +52,7 @@ def get_member(member_id):
 	
 
 @app.route('/member',methods=['POST'])
+@protected
 def add_member():
 	new_member_data = request.get_json()
 	name = new_member_data['name']
@@ -54,6 +70,7 @@ def add_member():
 
 
 @app.route('/member/<int:member_id>',methods=['PUT','PATCH'])
+@protected
 def edit_member(member_id):
 	member_data = request.get_json()
 	name = member_data['name']
@@ -74,8 +91,13 @@ def edit_member(member_id):
 
 
 @app.route('/member/<int:member_id>',methods=['DELETE'])
+@protected
 def delete_member(member_id):
-	return "This deletes a member by id"
+	db = get_db()
+	db.execute('delete from members where id =?',[member_id])
+	db.commit()
+
+	return jsonify({'message':'The record has been deleted'})
 
 
 if __name__ == '__main__':
